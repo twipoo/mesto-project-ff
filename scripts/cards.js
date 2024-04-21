209d487d7e12a3d580d7992539dde8dd79c9991b
Twipo <skobkatwipo@yandex.ru>
2024-04-21 20:25:13 +0700

    first-cmmit

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Зеленый масив гор с озером по центру , и немного снегу вокруг озера",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Не замершая река в заснеженном лесу",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Вечерний вид на спальный район, советской постройки.",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Фото у подножья вулкана, где еще растет зелень",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Желеная дорога скрывающия в горизон. По обе стороны лес",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Скалистый берег Байкала зимой",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
-
+const templateCard = document.querySelector('#card-template'); // Выбор Template
 // @todo: DOM узлы
-
+const list = document.querySelector('.places__list'); // Выбор элемента ul листа 
+const places__item = templateCard.content.querySelector('.places__item'); // выбор Элемент tempalte. li
+const nameCard = templateCard.content.querySelector('.card__title'); //выбор Card Title в li
+const img = templateCard.content.querySelector('.card__image'); // выбор img
 // @todo: Функция создания карточки
-
+function addCards (){
+    initialCards.forEach(function (initialCard) {
+        nameCard.textContent = initialCard.name;
+        img.src = initialCard.link;
+        img.alt = initialCard.alt;
+        let cloneCardPattern = places__item.cloneNode(true);
+        list.appendChild(cloneCardPattern);
 // @todo: Функция удаления карточки
-
+        const buttonsDell = document.querySelectorAll('.card__delete-button');// выбор кнопок удаления
+        function delitecard (e){
+          const currentButton =e.currentTarget;
+          currentButton.parentElement.remove();
+          }
+          buttonsDell.forEach(button => {
+            button.addEventListener('click', delitecard)
+          })
+    })
+    }
 // @todo: Вывести карточки на страницу
+addCards ();
