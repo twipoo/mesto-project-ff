23d27e5055eac894a98d6f36261ffc73d2a68e98
Twipo <skobkatwipo@yandex.ru>
2024-04-22 19:43:22 +0700

    three commiit

--- scripts/index.js
+++ scripts/index.js
@@ -4,34 +4,30 @@ const templateCard = document.querySelector('#card-template'); // Выбор Tem
 const list = document.querySelector('.places__list'); // Выбор элемента ul листа 
 const placesItem = templateCard.content.querySelector('.places__item'); // выбор Элемент tempalte. li
 // @todo: Функция создания карточки
-function createCard(initialCard) {
-  const card = placesItem.cloneNode(true);// создаем клон заготовки в перемунную card
-  const cardTitle = card.querySelector('.card__title');//выбираем в заготовке title
-  const cardImage = card.querySelector('.card__image');// Выбираем в заготовке img
-  
-  cardTitle.textContent = initialCard.name;
-  cardImage.src = initialCard.link;
-  cardImage.alt = initialCard.alt;
+function createCard(initialCard, deleteCard) {
+    const card = placesItem.cloneNode(true);
+    const cardTitle = card.querySelector('.card__title');
+    const cardImage = card.querySelector('.card__image');
+    const deleteButton = card.querySelector('.card__delete-button');
 
-  // @todo: Функция удаления карточки
-  const deleteButton = card.querySelector('.card__delete-button');// выбираем кнопки удаления карточки
+    cardTitle.textContent = initialCard.name;
+    cardImage.src = initialCard.link;
+    cardImage.alt = initialCard.alt;
 
-  deleteButton.addEventListener('click', function() {// Добавляет слушатель на карту.
-      card.remove();
-  });
-  
-  return card;
+    deleteButton.addEventListener('click', deleteCard);
+
+    return card;
 }
 // @todo: Вывести карточки на страницу
-
 function addCards() {
     initialCards.forEach(function(initialCard) {
-        const newCard = createCard(initialCard);
+        const newCard = createCard(initialCard, function() {
+            newCard.remove();
+        });
         list.appendChild(newCard);
     });
 }
 addCards();
-// Увидел коментарий на коде только после отправки практикум до сих пор не починили. только в историии  видно.
 
 
 
