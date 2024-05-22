6ef5d4afa5bb0433b5c90800daf0230c58a2a82c
Twipo <skobkatwipo@yandex.ru>
2024-05-22 18:06:02 +0700

    second commit pr 6

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 https://github.com/twipoo/mesto-project-ff.git
 
 
-npm run build:dev    //// development
-npm run build:prod   //// production
-npm run dev          //// live server
--- components/modal.js
+++ components/modal.js
@@ -1,24 +1,27 @@
-// modal.js
 
 export function openModal(modal) {
-  modal.classList.add("popup_is-animated");
-  modal.style.visibility = "visible";
-  modal.style.opacity = "1";
-  modal.style.pointerEvents = "all";
+  modal.classList.add("popup_is-opened");
+  modal.classList.remove("popup_is-animated");
   document.addEventListener("keydown", handleEscClose);
 }
 
 export function closeModal(modal) {
-  modal.style.opacity = "0";
-  modal.style.pointerEvents = "none";
+  modal.classList.add("popup_is-animated");
+  modal.classList.remove("popup_is-opened");
   document.removeEventListener("keydown", handleEscClose);
+}
+
+export function closePopupByOverlay(evt) {
+  if (evt.target === evt.currentTarget) {
+    closeModal(evt.currentTarget);
+  }
 }
 
 function handleEscClose(evt) {
   if (evt.key === "Escape") {
-    const openModal = document.querySelector(".popup_is-animated");
-    closeModal(openModal);
+    const openModal = document.querySelector(".popup_is-opened");
+    if (openModal) {
+      closeModal(openModal);
+    }
   }
 }
-
-//////
