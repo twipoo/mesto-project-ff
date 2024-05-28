090985c766355c7c4706fb22093f4d263c6de7ce
Twipo <skobkatwipo@yandex.ru>
2024-05-28 11:49:30 +0700

    new js file

---
+++ src/components/api.js
@@ -0,0 +1,14 @@
+ //token  '2b1d046a-7a38-44ae-adc1-5d0abf366ad9';
+ //group  'wff-cohort-15';
+
+function getUserInfo (){
+    return fetch('https://nomoreparties.co/v1/wff-cohort-15/cards', {
+  headers: {
+    authorization: '2b1d046a-7a38-44ae-adc1-5d0abf366ad9'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+  });
+}
